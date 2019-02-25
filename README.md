1ff3acd1d0ec09bba84d72daa1e182a49fab7ca5
lzakrzewski <lucaszz111@gmail.com>
2019-02-25 22:03:14 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# r2d2
+# r2d2 [![Build Status](https://travis-ci.org/lzakrzewski/r2d2.svg?branch=master)](https://travis-ci.org/lzakrzewski/r2d2)
 R2D2 - DeathStar API consumer
---
+++ tests/unit/TestTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase
+{
+    public function test_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
